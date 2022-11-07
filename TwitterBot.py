86e65877f4c9c101fb061eaac60c444e1d0f21c7
Petar Juric <petarjuricbepo@yahoo.com>
2022-11-06 20:24:54 -0800

    created dataframe bar chart figure

--- TwitterBot.py
+++ TwitterBot.py
@@ -4,7 +4,6 @@ import json
 import pandas as pd
 from textblob import TextBlob
 
-
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAJHbhgEAAAAAsQH3IOXabq3TwpPhwYM6EabBzx0%3DLxw4YBFnU3gWLu5wIkcetHq8KW6UVgj3bQ0Ad0UPTQOGDvRwtI"
@@ -13,9 +12,9 @@ search_url = "https://api.twitter.com/2/tweets/search/recent"
 
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-query_params = {'query': 'drug_use','tweet.fields': 'author_id',
-'max_results': 100,
-}
+query_params = {'query': 'drug_use', 'tweet.fields': 'author_id',
+                'max_results': 100,
+                }
 
 
 def bearer_oauth(r):
@@ -39,33 +38,26 @@ def connect_to_endpoint(url, params):
 
 def main():
     json_response = connect_to_endpoint(search_url, query_params)
-    #print(json.dumps(json_response, indent=4, sort_keys=True))
+    # print(json.dumps(json_response, indent=4, sort_keys=True))
     df = pd.DataFrame(response['data'])
     sentences = df['text']
     sentimentList = []
-    
 
     for sentence in sentences:
         sentimentList.append(TextBlob(sentence).sentiment.polarity)
-        
-         
+
     df['Sentiment'] = sentimentList
-    df.to_csv('response_python.csv') 
-      
-    
-        
-        
-       
+    df.to_csv('response_python.csv')
 
 
+    fig = df.plot(kind='bar', figsize=(20, 16), fontsize=26).get_figure()
+    fig.savefig('chart.pdf')
 
 
 if __name__ == "__main__":
     main()
 
-
-
 # Turn data into dataframes with pandas CHECK
 # Read text from dataframe and classify by sentiment CHECK
-# Insert senitment into dataframe 
-# Plot data on a bar chart 
+# Insert senitment into dataframe CHECK
+# Plot data on a bar chart
